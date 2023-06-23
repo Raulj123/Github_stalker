9c3a91d8c81f80f84c14291e591f6ef6b0649757
IamRaul <jarquinr121@gmail.com>
2023-06-22 19:12:06 -0700

    feat: first init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,69 @@
+<script lang='ts'>
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+  import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+  
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead"><LightSwitch /></svelte:fragment>
+	<svelte:fragment slot="trail">Bars here</svelte:fragment>
+</AppBar>
+
+    <div class="container">
+  <h1>Github stalker</h1>
+<img src="./stalker.png" alt="logo">
+    <div class="container_second">
+    <p class= "mt-5 mb-5 font-bold ">Enter a GitHub username, press 'Stalk,' and explore their profile stats and insights.</p>
+    </div>
+    </div>
+<slot />
+
+
+
+<style>
+  .container{
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    flex-direction:column;
+
+  }
+  .container img{
+    border-radius:15px;
+}
+  .container_second{
+    margin-top:5px;
+    text-align:center;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+  }
+  h1{
+    font-size:3rem;
+    margin-top:30px;
+    margin-bottom:30px;
+  }
+  footer{
+    text-align:center;
+  }
+  p{
+      max-width: 80%;
+  }
+ @media (max-width: 992px) {
+    h1{
+        font-size:1.7rem;
+    }
+    img{
+      height: 25%;
+      width: 25%;
+    }
+    p{
+      font-size:0.9rem;
+    }
+ 
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+
+
+<div class ="container">
+<form action="" method="POST">
+  <input type="text" name="user" placeholder="UserName" rows={1} required>
+       <button class="btn variant-filled-primary"type="submit">Stalk</button>
+</form>
+    </div>
+
+
+  <style>
+  .container{
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    display-direction:row;
+  }
+  .container input{
+    border-radius:10px;
+  }
+  .container button{
+    border-radius:10px;
+    padding:9px;
+    margin-left:5px;
+  }
+ @media (max-width: 992px) {
+    button{
+      padding:3px;
+    }
+    .container button{
+      padding:9px; 
+    }
+  }
+
+</style>
