0ace87366477181a9ee7bfc5d2775993cf4d4cd9
IamRaul <jarquinr121@gmail.com>
2023-06-23 17:30:14 -0700

    🐛 : fixed sending data back

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,31 @@
+export const user_info: any[] = [
+	{
+		avatar_url: 'https://avatars.githubusercontent.com/u/112786129?v=4',
+		url: 'https://api.github.com/users/Raulj123',
+		followers_url: 'https://api.github.com/users/Raulj123/followers',
+		following_url: 'https://api.github.com/users/Raulj123/following{/other_user}',
+		repos_url: 'https://api.github.com/users/Raulj123/repos',
+		name: 'Raul Jarquin',
+		company: null,
+		blog: 'linkedin.com/in/rauljarquin',
+		location: 'Southern California',
+		bio: "Computer Science at CSUF. '24",
+		twitter_username: null,
+		public_repos: 37,
+		public_gists: 0,
+		followers: 4,
+		following: 1,
+		created_at: '2022-09-04T00:10:25Z',
+		updated_at: '2023-06-20T05:05:09Z'
+	}
+];
+import type { PageServerLoad } from './$types';
+export const load: PageServerLoad = async () => {
+	return { user_info };
+};
+
 export const actions = {
-	getData: async ({ request, params }) => {
+	getData: async ({ request }) => {
 		const { user } = Object.fromEntries(await request.formData()) as {
 			user: string;
 		};
@@ -7,9 +33,11 @@ export const actions = {
 			const url = `https://api.github.com/users/${user}`;
 
 			const res = await fetch(url);
-			const data_user = await res.json();
-			console.log(data_user);
-			return data_user;
+			const user_data = await res.json();
+
+			user_info.splice(0);
+			user_info.push(user_data);
+			return user_data;
 		} catch (error) {
 			console.log('Error:', error);
 		}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
+  import { Avatar } from '@skeletonlabs/skeleton';
  export let data: PageData;
  
- console.log(data)
-</script>
+const location = data.user_info[0].location;
+ const name = data.user_info[0].name;
+ const bio = data.user_info[0].bio;
+ const avatar = data.user_info[0].avatar_url;
+ const followers = data.user_info[0].followers;
+const following = data.user_info[0].following 
 
+  
+   
+ 
+    
+  
+ console.log(location)
+</script>
+<h1>{location},{name},{bio},{avatar},{followers},{following}</h1>
 <div class ="container">
 <form action="?/getData" method="POST">
   <input type="text" name="user" class="variant-filled-primary" placeholder="UserName" rows={1} required>
        <button class="btn variant-filled-primary"type="submit">Stalk</button>
 </form>
     </div>
-
+<div class="card w-[75%] mx-auto mt-20">
+  <div class="header_contianer">
+  <Avatar src="{avatar}" width="w-32" rounded="rounded-full" />
+    </div>
+</div>
 
   <style>
   .container{
@@ -26,6 +43,11 @@
     border-radius:10px;
     padding:9px;
     margin-left:5px;
+  }
+  .header_contianer{
+    margin-left: 2rem;
+    display:flex;
+    display-direction: row;
   }
  @media (max-width: 992px) {
     button{
