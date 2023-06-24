de19f6a764cdf67bfed5f6d7b0550917a80341ba
IamRaul <jarquinr121@gmail.com>
2023-06-23 20:55:54 -0700

    feat: added half of card css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,25 +8,35 @@ const location = data.user_info[0].location;
  const avatar = data.user_info[0].avatar_url;
  const followers = data.user_info[0].followers;
 const following = data.user_info[0].following 
-
+const user = data.user_info[0].login
   
    
  
     
   
- console.log(location)
 </script>
-<h1>{location},{name},{bio},{avatar},{followers},{following}</h1>
 <div class ="container">
 <form action="?/getData" method="POST">
   <input type="text" name="user" class="variant-filled-primary" placeholder="UserName" rows={1} required>
        <button class="btn variant-filled-primary"type="submit">Stalk</button>
 </form>
     </div>
-<div class="card w-[75%] mx-auto mt-20">
-  <div class="header_contianer">
-  <Avatar src="{avatar}" width="w-32" rounded="rounded-full" />
-    </div>
+<div class=" Box card w-[50%] mx-auto mt-20">
+  <div class="Card_container">
+  <div class="First_col">
+  <img src="{avatar}" alt="{avatar}" class="mt-5 mb-5" />
+      <p>{followers}</p>
+      <p>{following}</p>
+      </div>
+    <div class="Second_col">
+        <h2 class="p-5 pb-0 font-bold ">{name}</h2>
+        <h3 class="p-5 pt-2 pb-0 font-light">{user}</h3>
+        <h2 class="p-5 pt-2">{bio}</h2>
+      </div>
+  
+
+      </div>
+
 </div>
 
   <style>
@@ -44,17 +54,42 @@ const following = data.user_info[0].following
     padding:9px;
     margin-left:5px;
   }
-  .header_contianer{
-    margin-left: 2rem;
-    display:flex;
-    display-direction: row;
+  .Card_container{
+    display:grid;
+  grid-template-columns: repeat(4, 1fr);
   }
+  .Second_col{
+    grid-col-start:2;
+    grid-column: span 3;
+  }
+  .First_col{
+    margin-left: 1rem;
+     grid-row-start: 1;
+    grid-column-start: span 1;
+  }
+  .First_col img{
+    width:90px;
+    height:90px;
+    border-radius:50%;
+  }
+  
+   
  @media (max-width: 992px) {
     button{
       padding:3px;
     }
     .container button{
       padding:9px; 
+    }
+       .First_col h2 {
+      font-size: 0.9rem;
+    }
+    .First_col img{
+      width: 60px;
+      height:60px;
+    }
+    .Box{
+      width: 70%;
     }
   }
 
